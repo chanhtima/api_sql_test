5648d7225691ad0cbfda298afd4858ce667967d3
Chanthima Tosungneon <chanthima.t@astartechs.com>
2024-08-16 10:56:54 +0700

    api

--- config/db.js
+++ config/db.js
@@ -1,19 +1,16 @@
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-const db = pgp({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
 // const db = pgp({
-//     connectionString: process.env.POSTGRES_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_DATABASE,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
 // });
+const db = pgp({
+   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
 db.connect()
     .then(() => {
         console.log("Database connected successfully");
